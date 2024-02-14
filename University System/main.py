187ecffa0017a47aec22b0c598e79c3d9bb18fe1
XxMohamedxX867 <70481848+XxMohamedxX867@users.noreply.github.com>
2024-02-13 17:32:43 -0800

    Update main.py

--- University System/main.py
+++ University System/main.py
@@ -1,255 +1,219 @@
-kerollos = {"Name": "kerollos ashraf wadia",
-            "ID": "202102360",
-            "age": "19",
-            "college": "IT",
-            "email": "k.gerges2360@su.edu.eg",
-            "GPA": "4"
-            }
-osama = {"Name": "Osama ahmed mahmoud",
-         "ID": "202102355",
-         "age": "18",
-         "college": "IT",
-         "email": "o.ahmed2355@su.edu.eg",
-         "GPA": "4"
-         }
-gebaly = {"Name": "Mohamed adel gebaly",
-          "ID": "202102357",
-          "age": "19",
-          "college": "IT",
-          "email": "m.oraby2357@su.edu.eg",
-          "GPA": "4"
-          }
-ahmed = {"Name": "Ahmed mohamed reda",
-         "ID": "202103084",
-         "age": "19",
-         "college": "IT",
-         "email": "a.saleh3084@su.edu.eg",
-         "GPA": "4"
-         }
-students_list = []
-def add():
-    name = input("type your name>>")
+import sys
+
+Student_Data = []
+Admin_Data = [{"username":"Gebaly","password":"1234"}]
+id = 1
+#-------------------------------------------------------
+def add_student():
+    global id
+    name=input("enter student name: ")
+    while(True):
+        try:
+            age=int(input("enter student age: "))
+            break
+        except:
+            print("Please enter an integer value!!")
+
     while True:
         try:
-            ID = int(input("type your ID>>"))
+            gpa = float(input("enter current GPA: "))
+            break
         except:
-            print("Please enter a number!")
-            continue
-        else:
-            break
+            print("please enter a float value!!")
+
     while True:
-        try:
-            age = int(input("type your age>>"))
-        except:
-            print("Please enter a number!")
-            continue
-        else:
-            break
-    college = input("type your college>>")
-    email = input("type your email>>")
-    while True:
-        try:
-            GPA = float(input("type your GPA>>"))
-        except:
-            print("Please enter a number!")
-            continue
-        else:
-            break
-    new_password = input("Enter your new password>>")
-    new = {"Name": name,
-           "ID": ID,
-           "age": age,
-           "college": college,
-           "email": email,
-           "GPA": GPA
-           }
-
-    students_list.append(new)
-    print(students_list)
-
-def update(u):
-    while True:
-        print("(Name | ID | Age | College | E-mail | GPA)")
-        o = input("choose what to change or type 'exit': ")
-        if o == "name" or o == "Name":
-            students_list[0]["Name"] = input("Enter new name>>")
-            print("Name changed")
-            print(students_list[0].items())
-            continue
-        elif o == "ID" or o == "id":
-            students_list[0]["ID"] = input("Enter new ID>>")
-            print("ID changed")
-            print(students_list[0].items())
-            continue
-        elif o == "age" or o == "AGE":
-            students_list[0]["age"] = input("Enter new age>>")
-            print("age changed")
-            print(students_list[0].items())
-            continue
-        elif o == "COLLEGE" or o == "college":
-            students_list[0]["college"] = input("enter new college>>")
-            print("college changed")
-            print(students_list[0].items())
-            continue
-        elif o == "email" or o == "EMAIL":
-            students_list[0]["email"] = input("Enter new Email>>")
-            print("email changed")
-            print(students_list[0].items())
-            continue
-        elif o == "gpa" or o == "GPA":
-            students_list[0]["GPA"] = input("Enter new GPA>>")
-            print("GPA changed")
-            print(students_list[0].items())
-            continue
-        elif o == "exit" or o == "EXIT":
-            break
-        else:
-            print("wrong type!!")
-            continue
-
-
-def checker(username, password):
-    try:
-        if (username == "kerollos" and password == "1234") or (username == "osama" and password == "1234") or (
-                username == "gebaly" and password == "1234") or (username == "ahmed" and password == "1234") or (
-                username == new["Name"] and password == new_password):
-            print("successful login")
-            return True
-    except:
-        print("Please enter your username and password again!")
-        return False
-
-
-def user(x):
-    while True:
-        print("1.view your information")
-        print("2.Exit")
-        student_menu = int(input("Enter the number>>"))
-        try:
-            if student_menu == 1:
-                print(eval("{}.items()".format(x)))
-        except:
-            print(new)
-        if student_menu == 2:
-            break
-        if (student_menu < 1) and (student_menu > 2):
-            print("please enter a number from menu!")
-            continue
-
-
-while True:
-    print("1.admin")
-    print("2.student")
-    print("3.Exit")
-    main_menu = input("Enter the number>>")
-
-    if main_menu == "1":
-        print("(admin)")
-        while True:
-            admin_name = input("Enter user name>>")
-            admin_password = input("Enter password>>")
-            if admin_name == "mazen" and admin_password == "1234":
-                print("successful login")
+        chk_not_joined_if = True
+        for i in Student_Data:  # for duplication (check all dicts without find the same id)
+            if i["ID"] == id:
+                chk_not_joined_if=False
+                id+=1
                 break
-            else:
-                print("invalid! Please enter user name and password again")
-                continue
-        while True:
-            print("1.add student")
-            print("2.update on student information")
-            print("3.delete student")
-            print("4.exit")
-            admin_menu = input("Enter the number>>")
-            if admin_menu == "1":
-                add()
+        if(chk_not_joined_if):
+            break
+    student_id = id
 
-            elif admin_menu == "2":
-                print("1.kerollos")
-                print("2.osama")
-                print("3.gebaly")
-                print("4.ahmed")
-                try:
-                    print("5.",students_list[0]["Name"],sep="")
-                except:
-                    print("")
-                try:
-                    print("6.",students_list[1]["Name"],sep="")
-                except:
-                    print("")
-                try:
-                    print("7.",students_list[2]["Name"],sep="")
-                except:
-                    print("")
-                update_menu = input("type a number of name you want to change>>")
-                if update_menu == "1":
-                    u = "kerollos"
-                    update(u)
-                elif update_menu == "2":
-                    u = "osama"
-                    update(u)
-                elif update_menu == "3":
-                    u = "gebaly"
-                    update(u)
-                elif update_menu == "4":
-                    u = "ahmed"
-                    update(u)
-                elif update_menu == "5":
-                    l = students_list[0]
-                    u = str(l["Name"])
-                    update(u)
-                elif update_menu == "6":
-                    u = students_list[1]["Name"]
-                    update(u)
-                elif update_menu == "7":
-                    u = students_list[2]["Name"]
-                    update(u)
-            elif admin_menu == "3":
-                print("1.kerollos")
-                print("2.osama")
-                print("3.gebaly")
-                print("4.ahmed")
-                try:
-                    print("5." + str(new['Name']))
-                except:
-                    print("")
-                delete_menu = input("Enter the number of name you want to delete>> ")
-                if delete_menu == "1":
-                    kerollos.clear()
-                    print("deleting kerollos..")
-                    print("kerollos deleted")
-                elif delete_menu == "2":
-                    osama.clear()
-                    print("deleting osama..")
-                    print("osama deleted")
-                elif delete_menu == "3":
-                    gebaly.clear()
-                    print("deleting gebaly..")
-                    print("gebaly deleted")
-                elif delete_menu == "4":
-                    ahmed.clear()
-                    print("deleting ahmed..")
-                    print("ahmed deleted")
-                elif delete_menu == "5":
-                    try:
-                        print("deleting " + new["Name"] + "..")
-                        print(new["Name"] + " deleted")
-                        new.clear()
-                    except:
-                        print("Nothing to delete!")
-            elif admin_menu == "4":
-                break
-            else:
-                print("please enter a number from the menu!")
-                continue
-    if main_menu == "2":
-        while True:
-            student_name = input("Enter your username>>")
-            student_password = input("Enter your password>>")
-            if checker(student_name, student_password):
-                break
-        user(student_name)
-    if main_menu == "3":
-        break
+    password = input("Enter Password: ")
+
+    Student_Data.append({"ID":student_id , "Name":name , "Age":age , "GPA":gpa , "username":name+str(student_id), "password":password})
+    print(name + " has been added!!")
+def edit_student():
+    print(Student_Data)
+    edit_id = int(input("Enter Current Student ID: "))
+    cnt = 0
+    chk_no_current_id = True
+    for i in Student_Data:
+        if i["ID"] == edit_id:
+            chk_no_current_id = False
+            name = input("enter New student name: ")
+            while (True):
+                age = int(input("enter New student age: "))
+                if type(age) is not int:
+                    print("Please enter an integer value!!")
+                    continue
+                else:
+                    break
+            while (True):
+                gpa = float(input("enter a New GPA: "))
+                if type(gpa) is not float:
+                    print("please enter a float value!!")
+                    continue
+                else:
+                    break
+            while (True):
+                chk_id = True
+                NEW_ID = int(input("enter New ID: "))
+                if type(NEW_ID) is not int:
+                    print("please enter a float value!!")
+                    continue
+                for i in Student_Data: # for duplication
+                    if i["ID"] == NEW_ID:
+                        print("This ID is already Used!!")
+                        chk_id = False
+                        break
+                if chk_id == True:
+                    break
+            Student_Data[cnt]={"ID": NEW_ID, "Name": name, "Age": age, "GPA": gpa}
+        else:
+            cnt += 1
+    if chk_no_current_id:
+        print("No Editing <3")
     else:
-        print("please enter a number from the list!")
-        continue
+        print("Done  >_<  <3")
+
+def delete_student():
+    print(Student_Data)
+    cnt = 0
+    chk_no_current_id = True
+    delete_id = int(input("Enter Student ID to delete or zero to exit: "))
+    for i in Student_Data:
+        if i["ID"] == delete_id:
+            chk_no_current_id = False
+            confirm_delete=input("Press Y to confirm or N to cancel: ")
+            if confirm_delete == "Y" or confirm_delete == "y":
+                del Student_Data[cnt]
+                chk_delete=True
+            else:
+                chk_delete = False
+        else:
+            cnt += 1
+    if (chk_no_current_id == True) or (chk_delete == False) :
+        print("No Deleting <3")
+    elif chk_delete:
+        print("Done  >_<  <3")
+def display_admin():
+    print("Students Database XD")
+    for i in Student_Data:
+        for key , value in i.items():
+            print(key ," --> ",value)
+        print("----------------------------")
+
+def give_access():
+    print(Student_Data)
+    cnt = 0
+    chk_no_current_id = True
+    access_id = int(input("Enter Student ID to Give Access or zero to exit: "))
+    for i in Student_Data:
+        if i["ID"] == access_id:
+            chk_no_current_id = False
+            confirm_access = input("Press Y to confirm or N to cancel: ")
+            if confirm_access == "Y" or confirm_access == "y":
+                Admin_Data.append(Student_Data[cnt])
+                print(f"Now {Student_Data[cnt]['Name']} Have direct access to the Admin Page !")
+        else:
+            cnt += 1
+    if chk_no_current_id:
+        print("You Did Nothing!")
+def admin():
+    username = input("Username: ")
+    password = input("Password: ")
+    for i in Admin_Data:
+        if i["username"] == username and i["password"] == password:
+            while(True):
+                print("1- Add Student")
+                print("2- Edit on Student Information")
+                print("3- Delete Student")
+                print("4- Display Students Information")
+                print("5- Give Access to Student (staff member)")
+                print("6- Back")
+                try:
+                    start_num = int(input("Enter a Number: "))
+                    if start_num == 1:
+                        add_student()
+                    elif start_num == 2:
+                        edit_student()
+                    elif start_num == 3:
+                        delete_student()
+                    elif start_num == 4:
+                        display_admin()
+                    elif start_num == 5:
+                        give_access()
+                    elif start_num == 6:
+                        start()
+                except:
+                    print("Please enter Number of these!")
+
+    print("Wrong password or username!")
+    admin()
+
+
+#-------------------------------------------------------
+def student():
+    print("Username = Name + ID")
+    username = input("Username: ")  # username is ( name + id )
+    password = input("Password: ")
+    cnt = 0
+    for i in Student_Data:
+        if i["username"] == username and i["password"] == password:
+            while (True):
+                print("1- View Information")
+                print("2- Change Password")
+                print("3- Back")
+                try:
+                    start_num = int(input("Enter a Number: "))
+                    if start_num == 1:
+                        display_student(i)
+                    elif start_num == 2:
+                        chanege_password(i,cnt)
+                    elif start_num == 3:
+                        start()
+                except:
+                    print("Please enter Number of these!")
+
+        else:
+            cnt += 1
+
+    print("Wrong password or username!")
+    student()
+
+def display_student(i):
+    for key, value in i.items():
+        print(key, " --> ", value)
+    print("---------------------------------")
+def chanege_password(i,cnt):
+    chk_old_pass = input("Enter your old Password: ")
+    new_pass = input("Enter a new password: ")
+    if chk_old_pass != i["password"]:
+        print("Wrong password!")
+        chanege_password(i,cnt)
+    else:
+        Student_Data[cnt]["password"] = new_pass
+#-------------------------------------------------------
+def start():
+    while(True):
+        print("1- Admin Page")
+        print("2- Student Page")
+        print("3- Exit")
+        try:
+            start_num = int(input("Enter a Number: "))
+            if start_num == 1:
+                admin()
+            elif start_num == 2:
+                student()
+            elif start_num == 3:
+                print("Good Bye <3")
+                return 0                                #problem
+        except:
+            print("Please enter Number of these!")
+start()
+
+
